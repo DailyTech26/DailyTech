1e5ceda07e8600b8393ad42e4e91b2212cbe6924
DailyTech <oyeniranademayowq@gmail.com>
2025-07-09 18:02:42 -0700

    firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,19 +1,12 @@
-// src/firebase.js
-import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";  
-
-const firebaseConfig = {
-  apiKey: "AIzaSyCnpXqKasnM2hqb-Q2BvzRuhDvDD4i-4e4",
-  authDomain: "dailytech-b0f69.web.app",
-  projectId: "dailytech-b0f69",
-  storageBucket: "dailytech-b0f69.appspot.com", 
-  messagingSenderId: "62116729328",
-  appId: "1:62116729328:web:ccdf43e2c3df8f8e9ecd35"
-};
-
-const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-const db = getFirestore(app);  
-
-export { auth, db }; 
+{
+  "hosting": {
+    "public": "build",
+    "ignore": ["firebase.json", "**/.*", "**/node_modules/**"],
+    "rewrites": [
+      {
+        "source": "**",
+        "destination": "/index.html"
+      }
+    ]
+  }
+}
