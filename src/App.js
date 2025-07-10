b21e86fe2a9cc0ee806aeae78d69f9509d662aac
DailyTech <oyeniranademayowq@gmail.com>
2025-07-10 05:33:39 -0700

    App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,10 @@
-// src/App.js (for GitHub Pages)
 import React from "react";
-import { HashRouter as Router, Routes, Route } from "react-router-dom";
-import { AuthProvider } from "./context/AuthContext";
-
-import LoginPage from "./pages/LoginPage";
-import SignupPage from "./pages/SignupPage";
-import Dashboard from "./pages/Dashboard";
-import AdminPanel from "./pages/AdminPanel";
 
 function App() {
   return (
-    <AuthProvider>
-      <Router>
-        <Routes>
-          <Route path="/" element={<LoginPage />} />
-          <Route path="/signup" element={<SignupPage />} />
-          <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/admin" element={<AdminPanel />} />
-        </Routes>
-      </Router>
-    </AuthProvider>
+    <div>
+      <h1>Welcome to DailyTech!</h1>
+    </div>
   );
 }
 
